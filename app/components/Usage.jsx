ada2c55281d44d5b05d920690dbc3c61c15a187d
Craig Michael Thompson <craig@craigsworks.com>
2015-03-11 00:50:03 +0000

    Split usage functionality into it's own app component

---
+++ app/components/Usage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ContextMixin from '../mixins/ContextMixin.js';
+
+export default React.createClass({
+	mixins: [ContextMixin],
+
+	cursors: {
+		usage: ['usage']
+	},
+
+	render() {
+		return (
+			<div>
+				<h1>Server Statistics</h1>
+				<dl>
+					<dt>CPU Usage</dt>
+					<dd>{this.state.usage.cpu.toFixed(2)}%</dd>
+
+					<dt>Memory Usage</dt>
+					<dd>{(this.state.usage.memory / 1E6).toFixed(0)}MB</dd>
+				</dl>
+			</div>
+		);
+	}
+});
